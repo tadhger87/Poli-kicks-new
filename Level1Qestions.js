0a90df010584c9f5a265d1964ecf64ef9b8a8753
PiotrPiotr <psmail82@gmail.com>
2014-12-13 16:47:19 +0000

    displaying questions in html element
    need to work out how to go to next question upon button click, itereate
    the array of questions

---
+++ Level1Qestions.js
@@ -0,0 +1,85 @@
+var questionArray = new Array();
+questionArray[1]="If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations."
+questionArray[2]="I'd always support my country, whether it was right or wrong."
+questionArray[3]="No one chooses his or her country of birth, so it's foolish to be proud of it."
+questionArray[4]="Our race has many superior qualities, compared with other races."
+questionArray[5]="The enemy of my enemy is my friend."
+
+	  </br>
+	  <input type="radio" name="answer" value="Strongly Disagree">Strongly Disagree</br>
+      <input type="radio" name="answer" value="Disagree">Disagree</br>
+	  <input type="radio" name="answer" value="Agree">Agree</br>
+      <input type="radio" name="answer" value="Strongly Agree">Strongly Agree</br>
+	  </div>
+	  <input type="submit" onclick="goToPrevious()" value="Previous Question" />
+	  <input type="submit" onclick="goToNext()" value="Next Question" />
+	  </form>
+
+
+
+
+
+
+
+/*
+questionArray[1]="Military action that defies international law is sometimes justified."
+questionArray[1]="There is now a worrying fusion of information and entertainment."
+questionArray[1]="People are ultimately divided more by class than by nationality."
+questionArray[1]="Controlling inflation is more important than controlling unemployment."
+questionArray[1]="Because corporations cannot be trusted to voluntarily protect the environment, they require regulation."
+
+questionArray[1]=""from each according to his ability, to each according to his need" is a fundamentally good idea."
+questionArray[1]="It's a sad reflection on our society that something as basic as drinking water is now a bottled, branded consumer product."
+questionArray[1]="Land shouldn't be a commodity to be bought and sold."
+questionArray[1]="It is regrettable that many personal fortunes are made by people who simply manipulate money and contribute nothing to their society."
+questionArray[1]="Protectionism is sometimes necessary in trade."
+
+questionArray[1]="The only social responsibility of a company should be to deliver a profit to its shareholders."
+questionArray[1]="The rich are too highly taxed."
+questionArray[1]="Those with the ability to pay should have the right to higher standards of medical care ."
+questionArray[1]="Governments should penalise businesses that mislead the public."
+questionArray[1]="A genuine free market requires restrictions on the ability of predator multinationals to create monopolies."
+
+questionArray[1]="The freer the market, the freer the people."
+questionArray[1]="Abortion, when the woman's life is not threatened, should always be illegal."
+questionArray[1]="All authority should be questioned."
+questionArray[1]="An eye for an eye and a tooth for a tooth.</p>
+questionArray[1]="Taxpayers should not be expected to prop up any theatres or museums that cannot survive on a commercial basis.</p>
+questionArray[1]="Schools should not make classroom attendance compulsory.</p>
+questionArray[1]="All people have their rights, but it is better for all of us that different sorts of people should keep to their own kind.</p>
+questionArray[1]="Good parents sometimes have to spank their children.</p>
+questionArray[1]="It's natural for children to keep some secrets from their parents.</p>
+questionArray[1]="Possessing marijuana for personal use should not be a criminal offence.</p>
+questionArray[1]="The prime function of schooling should be to equip the future generation to find jobs.</p>
+questionArray[1]="People with serious inheritable disabilities should not be allowed to reproduce.</p>
+questionArray[1]="The most important thing for children to learn is to accept discipline.</p>
+questionArray[1]="There are no savage and civilised peoples; there are only different cultures.</p>
+questionArray[1]="Those who are able to work, and refuse the opportunity, should not expect society's support.</p>
+questionArray[1]="When you are troubled, it's better not to think about it, but to keep busy with more cheerful things.</p>
+questionArray[1]="First-generation immigrants can never be fully integrated within their new country.</p>
+questionArray[1]="What's good for the most successful corporations is always, ultimately, good for all of us.</p>
+questionArray[1]="No broadcasting institution, however independent its content, should receive public funding.</p>
+questionArray[1]="Our civil liberties are being excessively curbed in the name of counter-terrorism.</p>
+questionArray[1]="A significant advantage of a one-party state is that it avoids all the arguments that delay progress in a democratic political system.</p>
+questionArray[1]="Although the electronic age makes official surveillance easier, only wrongdoers need to be worried.</p>
+questionArray[1]="The death penalty should be an option for the most serious crimes.</p>
+questionArray[1]="In a civilised society, one must always have people above to be obeyed and people below to be commanded.</p>
+questionArray[1]="Abstract art that doesn't represent anything shouldn't be considered art at all.</p>
+questionArray[1]="In criminal justice, punishment should be more important than rehabilitation."
+questionArray[1]="It is a waste of time to try to rehabilitate some criminals."
+questionArray[1]="The businessperson and the manufacturer are more important than the writer and the artist."
+questionArray[1]="Mothers may have careers, but their first duty is to be homemakers."
+questionArray[1]="Multinational companies are unethically exploiting the plant genetic resources of developing countries."
+questionArray[1]="Making peace with the establishment is an important aspect of maturity."
+questionArray[1]="Astrology accurately explains many things."
+questionArray[1]="You cannot be moral without being religious."
+questionArray[1]="Charity is better than social security as a means of helping the genuinely disadvantaged."
+questionArray[1]="Some people are naturally unlucky."
+questionArray[1]="It is important that my child's school instills religious values."
+questionArray[1]="Sex outside marriage is usually immoral."
+questionArray[1]="A same sex couple in a stable, loving relationship, should not be excluded from the possibility of child adoption."
+questionArray[1]="Pornography, depicting consenting adults, should be legal for the adult population."
+questionArray[1]="What goes on in a private bedroom between consenting adults is no business of the state."
+questionArray[1]="No one can feel naturally homosexual."
+questionArray[1]="These days openness about sex has gone too far."
+*/
---
+++ js/Level1.js
@@ -0,0 +1,11 @@
+var questionArray = ["If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations.", "I'd always support my country, whether it was right or wrong.", "No one chooses his or her country of birth, so it's foolish to be proud of it.", "Our race has many superior qualities, compared with other races.", "The enemy of my enemy is my friend."];
+function dispQuest(){
+	var questionArray = ["If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations.", "I'd always support my country, whether it was right or wrong.", "No one chooses his or her country of birth, so it's foolish to be proud of it.", "Our race has many superior qualities, compared with other races.", "The enemy of my enemy is my friend."];
+
+	//document.getElementById("singleQuestion").innerHTML=questionArray[0];
+		//for(i = 0; i<questionArray.length; i++){
+		i = 0;
+		document.getElementById("singleQuestion").innerHTML=questionArray[i];
+		//i++;
+			//var disp = questionArray[0];
+			}
